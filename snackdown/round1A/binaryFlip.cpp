fafe86ec05f2daf4c8822d19d73cc69ab10517b9
HOTSONHONET <hotsonhonet@gmail.com>
2021-10-23 20:31:08 +0530

    done with 5 problems

--- snackdown/round1A/binaryFlip.cpp
+++ snackdown/round1A/binaryFlip.cpp
@@ -237,49 +237,56 @@ ll gcd(ll a, ll b)
 
 void solve()
 {
-    ll n, ans1 = 0, ans2 = 0, d2, d1;
-    ll m = INT_MAX;
-    cin >> n;
-    vector<ll> a(n);
-    for (ll i = 0; i < n; i++)
+
+    ll n, k;
+    cin >> n >> k;
+    if (k == 0)
     {
-        cin >> a[i];
-    }
-
-    sort(a.begin(), a.end());
-
-    ll l = 1, r = n - 2;
-
-    if (n == 2)
-    {
-        cout << 0 << endl;
+        cout << "YES"
+             << "\n"
+             << 0 << endl;
         return;
     }
-
-    if (n == 3)
+    if (k & 1)
     {
-        cout << min(a[1] - a[0], a[2] - a[1]) << endl;
-        return;
+        ll tmp = 0;
+        for (ll i = 31; i >= 0; i--)
+        {
+            if (k & (1 << i))
+            {
+                tmp = i + 1;
+                break;
+            }
+        }
+
+        cout << "YES"
+             << "\n";
+        k = (k + (1 << tmp) - 1) / 2;
+        cout << tmp << "\n";
+        ll ans = 1;
+        vector<int> v;
+        for (ll i = tmp - 2; i >= 0; i--)
+        {
+            if (k & (1 << i))
+            {
+                v.push_back((ans));
+                ans += (1 << i);
+            }
+            else
+            {
+                ans -= (1 << i);
+                v.push_back(ans);
+            }
+        }
+
+        for (int i = tmp - 2; i >= 0; i--)
+            cout << v[i] << "\n";
+        cout << ans << endl;
     }
-
-    for (ll i = 0; i < n - 1; i++)
-        ans1 += abs(a[i] - a[(n - 1) / 2]);
-
-    for (ll i = 1; i < n; i++)
-        ans2 += abs(a[i] - a[1 + (n - 1) / 2]);
-
-    m = min(ans1, ans2);
-
-    while (l < r)
+    else
     {
-        d2 = a[n - 1] - a[l], d1 = a[r] - a[0];
-        m = min(m, abs(d1 - d2));
-        if (d1 < d2)
-            l++;
-        else
-            r--;
+        cout << "NO" << endl;
     }
-    cout << m << endl;
 }
 
 int main()
